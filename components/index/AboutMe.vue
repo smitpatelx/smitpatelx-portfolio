f66f1d11b3986a71c0643450a7b8c23c6963f42d
Smit Patel <smitbpatel1999@gmail.com>
2020-04-23 19:54:40 -0400

    About me component created

---
+++ components/index/AboutMe.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full flex justify-center items-center p-32">
+        <div class="w-1/2 flex flex-wrap justify-center items-start pl-20">
+            <h2 class="w-full text-left flex flex-wrap justify-start items-center">
+                <span class="mr-3 text-teal-400 text-2xl font-medium font-mono mt-1">01.</span>
+                <span class="text-gray-400 text-3xl font-bold">About Me</span>
+                <div class="bg-gray-600 w-full" style="padding-top:0.6px;"></div>
+            </h2>
+        </div>
+        <div class="w-1/2">
+            <img src="/logo.png" alt="SVG Art">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    
+</style>
