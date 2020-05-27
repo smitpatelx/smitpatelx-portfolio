e9db585f483f300544cc93871dfb45efb54be7ce
Smit Patel <smitbpatel1999@gmail.com>
2020-05-27 19:37:23 -0400

    Working Experience Section

--- components/index/Experience.vue
+++ components/index/Experience.vue
@@ -8,10 +8,10 @@
             </h2>
             <div class="w-full h-full flex flex-wrap mt-8">
                 <div class="w-full lg:w-1/5 text-gray-600 flex flex-wrap flex-row lg:flex-col text-center items-center justify-start font-mono tracking-tight">
-                    <button type="button" class="w-auto px-4 lg:px-0 lg:w-full border-l-2 border-blue-900 focus:border-teal-400 focus:outline-none btn-anim-1 focus:text-teal-400">Netdevv</button>
-                    <button type="button" class="w-auto px-4 lg:px-0 lg:w-full border-l-2 border-blue-900 focus:border-teal-400 focus:outline-none btn-anim-1 focus:text-teal-400">Netdevv</button>
+                    <button type="button" @click="change_company(1)" class="w-auto px-4 lg:px-0 lg:w-full border-l-2 border-blue-900 focus:border-teal-400 focus:outline-none btn-anim-1 focus:text-teal-400">Netdevv</button>
+                    <button type="button" @click="change_company(2)" class="w-auto px-4 lg:px-0 lg:w-full border-l-2 border-blue-900 focus:border-teal-400 focus:outline-none btn-anim-1 focus:text-teal-400">Netdevv</button>
                 </div>
-                <div class="w-full lg:w-4/5 pl-4 mt-6 lg:mt-0">
+                <div v-if="current_company==1" class="w-full lg:w-4/5 pl-4 mt-6 lg:mt-0">
                     <p class="text-xl font-medium text-gray-400">Full-stack Developer<span class="pl-2 text-teal-400">@ Netdevv</span></p>
                     <p class="text-sm text-gray-500 font-mono mt-3">Oct 2017 – Apr 2020</p>
                     <ul class="text-gray-500 pt-6 text-base">
@@ -21,13 +21,33 @@
                         <li>Maintaining GitHub repository issues, answering questions and merging pull requests.</li>
                     </ul>
                 </div>
+                <div v-if="current_company==2" class="w-full lg:w-4/5 pl-4 mt-6 lg:mt-0">
+                    <p class="text-xl font-medium text-gray-400">Project Lead Developer<span class="pl-2 text-teal-400">@ Netdevv</span></p>
+                    <p class="text-sm text-gray-500 font-mono mt-3">Oct 2017 – Apr 2020</p>
+                    <ul class="text-gray-500 pt-6 text-base">
+                        <li>Perform web development tasks utilizing PHP/NodeJs Framework, Web Services, and SQL Server</li>
+                        <li>Lead design and implementation of solutions for our corporate websites and microsites</li>
+                        <li>Design and evolve highly interactive user interfaces and HTML prototypes for web and email initiatives</li>
+                        <li>Work as part of an agile team fostering teamwork, mentoring others, and striving for overall excellence</li>
+                        <li>Utilize social media platforms, support advanced online Marketing activities, and integrate with internal systems.</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            current_company: 1
+        }
+    },
+    methods: {
+        change_company(val){
+            this.current_company = val;
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
